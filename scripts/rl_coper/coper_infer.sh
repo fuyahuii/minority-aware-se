764b2b1ad84e97cec6333aa20bf6e2006e329333
Yahui Fu <1457132017@qq.com>
2025-11-24 17:18:41 +0900

    update

--- scripts/rl_coper/coper_infer.sh
+++
@@ -1,9 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-python inference/eval_rl_coper.py \
-    --base_model "meta-llama/Meta-Llama-3-8B-Instruct" \
-    --model_dir "models_outputs_rl/coper" \
-    --tokenizer_name_or_path "models_outputs_rl/coper" \
-    --eval_batch_size 8 \
-    --max_new_tokens 200
-
